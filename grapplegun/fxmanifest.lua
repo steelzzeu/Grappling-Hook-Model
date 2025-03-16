95fab98eae49c9a6e2b7f04682c8d032724d29ab
Steelzz <65611601+steelzzeu@users.noreply.github.com>
2025-03-16 20:31:28 +0000

    Add files via upload

---
+++ grapplegun/fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Steelzz'
+description 'Replaces Compact Grenade Launcher with Custom Weapon'
+
+files {
+    'stream/w_lr_compactgl.ydr',
+    'stream/w_lr_compactgl_hi.ydr',
+    'stream/w_lr_compactgl.ytd',
+    'stream/w_lr_compactgl+hi.ytd',
+
+    'data/weapon.meta',
+    'data/weaponarchetypes.meta',
+    'data/weaponanimations.meta',
+    'data/weaponcomponents.meta',
+}
+
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weapon.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' 'data/weaponanimations.meta'
+data_file 'WEAPON_ARCHETYPES_FILE' 'data/weaponarchetypes.meta'
+data_file 'WEAPON_COMPONENTS_FILE' 'data/weaponcomponents.meta'
